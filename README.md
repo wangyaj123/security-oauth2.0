e90dd6303b983f0128e384bc1bd2bb676e3fa72b
wangyajing <2715956275@qq.com>
2019-08-14 16:16:09 +0800

    修改目录结构

---
+++ README.md
@@ -0,0 +1,6 @@
+# Getting Started
+springboot+security Oauth2实现token认证
+
+访问一下地址测试token
+localhost:8080/oauth/token?username=user_1&password=123456&grant_type=password&scop=select&client_id=client_2&client_secret=123456
+localhost:8080/order/demo?access_token=9e206db6-7449-4cb0-822e-8e9c0d8cd709
---
+++ src/main/java/com/wyj/oauth/security/SecurityApplication.java
@@ -0,0 +1,17 @@
+package com.wyj.oauth.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangyajing
+ * @date 2019-08-14
+ */
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+
+}
